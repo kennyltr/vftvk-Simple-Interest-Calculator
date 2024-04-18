de0484fee81cf224f1fe859641b36c949dc19266
kennyltr <kennyltr@hotmail.com>
2024-04-18 02:03:15 -0400

    updated

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function compute() {
         result.innerHTML = 
         "If you deposit $" + "<mark>" + principal + "</mark>" +
         ",\<br>at an interest rate of " + "<mark>" + rate + "</mark>" +
-        "%.\<br>You will receive an amount of " + "<mark>" + interest + "</mark>" +
+        "%.\<br>You will receive an amount of $" + "<mark>" + amount + "</mark>" +
         ",\<br>in the year " + "<mark>" + year + "</mark>" + "." + "\<br>";
     }
 }
